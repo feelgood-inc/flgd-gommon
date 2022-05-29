6b28398fd7854768f55b2c8860b40558f69b1402
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-05-29 01:34:29 -0400

    feat: consultation

--- models/consultation.go
+++ models/consultation.go
@@ -18,14 +18,14 @@ type Consultation struct {
 
 type ConsultationAggregated struct {
 	ID                   uint64       `json:"id"`
-	AppointmentID        uint64       `json:"appointment_id"`
+	Appointment          Appointment  `json:"appointment"`
 	Patient              Patient      `json:"patient"`
 	Practitioner         Practitioner `json:"practitioner"`
 	Status               string       `json:"status"`
 	ElapsedTimeInSeconds int64        `json:"elapsed_time_in_seconds"`
 	PaymentStatus        string       `json:"payment_status"`
-	CreatedAt            *time.Time   `json:"created_at"`
-	UpdatedAt            *time.Time   `json:"updated_at"`
 	PractitionerUID      string       `json:"practitioner_uid"`
 	PatientUID           string       `json:"patient_uid"`
+	CreatedAt            *time.Time   `json:"created_at"`
+	UpdatedAt            *time.Time   `json:"updated_at"`
 }
