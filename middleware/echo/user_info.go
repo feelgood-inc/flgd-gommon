89acfa95cbb632d6a050a83b0ef292551f4db43a
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-06-05 01:52:15 -0400

    reafactor: wraps as middlewarefunc

--- middleware/echo/user_info.go
+++ middleware/echo/user_info.go
@@ -6,18 +6,24 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func SetUserInfo(ctx echo.Context) {
-	authHeader := ctx.Request().Header.Get("Authorization")
-	var claims models.FeelgoodJWTCustomClaims
-	decodedToken, err := jwt.ParseWithClaims(authHeader, claims, nil)
-	if err != nil {
-		return
+func SetUserInfo() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ctx echo.Context) error {
+			authHeader := ctx.Request().Header.Get("Authorization")
+			var claims models.FeelgoodJWTCustomClaims
+			decodedToken, err := jwt.ParseWithClaims(authHeader, claims, nil)
+			if err != nil {
+				return ctx.JSON(401, map[string]string{"error": "Invalid token"})
+			}
+			user := models.User{
+				UID:      decodedToken.Claims.(*models.FeelgoodJWTCustomClaims).UID,
+				Email:    decodedToken.Claims.(*models.FeelgoodJWTCustomClaims).Email,
+				Provider: decodedToken.Claims.(*models.FeelgoodJWTCustomClaims).Provider,
+				Type:     decodedToken.Claims.(*models.FeelgoodJWTCustomClaims).Type,
+			}
+			ctx.Set("user_info", user)
+
+			return next(ctx)
+		}
 	}
-	user := models.User{
-		UID:      decodedToken.Claims.(*models.FeelgoodJWTCustomClaims).UID,
-		Email:    decodedToken.Claims.(*models.FeelgoodJWTCustomClaims).Email,
-		Provider: decodedToken.Claims.(*models.FeelgoodJWTCustomClaims).Provider,
-		Type:     decodedToken.Claims.(*models.FeelgoodJWTCustomClaims).Type,
-	}
-	ctx.Set("user_info", user)
 }
