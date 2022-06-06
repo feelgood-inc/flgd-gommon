3b28ee193d6af5043e5e85d7310094dcc43ec52c
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-06-05 21:40:11 -0400

    refactor: implement standard claims

--- models/jwt.go
+++ models/jwt.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/golang-jwt/jwt"
+
 type FeelgoodJWTPayload struct {
 	UID       string   `json:"uid"`
 	Email     string   `json:"email"`
@@ -14,9 +16,5 @@ type FeelgoodJWTCustomClaims struct {
 	Type     string `json:"type"`
 	Provider string `json:"provider"`
 	UID      string `json:"uid"`
-}
-
-func (f FeelgoodJWTCustomClaims) Valid() error {
-	//TODO implement me
-	panic("implement me")
+	jwt.StandardClaims
 }
