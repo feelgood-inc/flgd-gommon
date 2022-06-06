ec20206c84fda3e4e125a6317241c39cd3cff2e9
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-06-05 22:23:02 -0400

    fix: changes parameter

--- middleware/echo/user_info.go
+++ middleware/echo/user_info.go
@@ -10,8 +10,7 @@ func SetUserInfo(withKey string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
 			authHeader := ctx.Request().Header.Get("Authorization")
-			var claims models.FeelgoodJWTCustomClaims
-			decodedToken, err := jwt.ParseWithClaims(authHeader, claims, nil)
+			decodedToken, err := jwt.ParseWithClaims(authHeader, &models.FeelgoodJWTCustomClaims{}, nil)
 			if err != nil {
 				return ctx.JSON(401, map[string]string{"error": "Invalid token"})
 			}
