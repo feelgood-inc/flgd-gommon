5aa05f47d60124921b185bce3cb54ab80f67ef1f
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-06-05 22:48:21 -0400

    fix: implements func

--- middleware/echo/user_info.go
+++ middleware/echo/user_info.go
@@ -10,7 +10,10 @@ func SetUserInfo(withKey string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
 			authHeader := ctx.Request().Header.Get("Authorization")
-			decodedToken, err := jwt.ParseWithClaims(authHeader, &models.FeelgoodJWTClaims{}, nil)
+			decodedToken, err := jwt.ParseWithClaims(authHeader, &models.FeelgoodJWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+				println("token: ", token)
+				return []byte("AllYourBase"), nil
+			})
 			if err != nil {
 				return ctx.JSON(401, map[string]string{"error": "Invalid token"})
 			}
