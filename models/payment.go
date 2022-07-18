04a9bb0e32fa44dd1d6b905ebc1a84af0f51265a
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-07-17 23:55:50 -0400

    feat: changes Optional field in CreatePaymentRequest struct

--- models/payment.go
+++ models/payment.go
@@ -1,18 +1,18 @@
 package models
 
 type CreatePaymentRequest struct {
-	CommerceOrder   string  `json:"commerce_order"`
-	Subject         string  `json:"subject"`
-	Currency        string  `json:"currency"`
-	Amount          float64 `json:"amount"`
-	Email           string  `json:"email"`
-	PaymentMethod   int     `json:"payment_method"`
-	URLConfirmation string  `json:"url_confirmation"`
-	URLReturn       string  `json:"url_return"`
-	Optional        string  `json:"optional"`
-	Timeout         int     `json:"timeout"`
-	MerchantID      string  `json:"merchant_id"`
-	PaymentCurrency string  `json:"payment_currency"`
+	CommerceOrder   string              `json:"commerce_order"`
+	Subject         string              `json:"subject"`
+	Currency        string              `json:"currency"`
+	Amount          float64             `json:"amount"`
+	Email           string              `json:"email"`
+	PaymentMethod   int                 `json:"payment_method"`
+	URLConfirmation string              `json:"url_confirmation"`
+	URLReturn       string              `json:"url_return"`
+	Optional        GatewayOptionalData `json:"optional"`
+	Timeout         int                 `json:"timeout"`
+	MerchantID      string              `json:"merchant_id"`
+	PaymentCurrency string              `json:"payment_currency"`
 }
 
 type CreatePaymentResponse struct {
