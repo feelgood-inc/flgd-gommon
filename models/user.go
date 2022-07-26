ff0accdd10e878c4fe85d5fc53bbe139bb875819
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-07-25 20:33:10 -0400

    feat: modifies usergp

--- models/user.go
+++ models/user.go
@@ -1,9 +1,15 @@
 package models
 
 type User struct {
-	UID      string   `json:"uid"`
-	Email    string   `json:"email"`
-	Provider string   `json:"provider"`
-	Type     string   `json:"type"`
-	Roles    []string `json:"roles"`
+	UID            string   `json:"uid"`
+	FirstName      string   `json:"first_name"`
+	SecondName     string   `json:"second_name"`
+	LastName       string   `json:"last_name"`
+	SecondLastName string   `json:"second_last_name"`
+	Gender         string   `json:"gender"`
+	NationalID     string   `json:"national_id"`
+	Email          string   `json:"email"`
+	Provider       string   `json:"provider"`
+	Type           string   `json:"type"`
+	Roles          []string `json:"roles"`
 }
