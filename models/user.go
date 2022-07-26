364dfceef2d12a20eb86c92e3222ba1fe2f14a27
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-07-25 22:26:03 -0400

    feat: nil values for user model

--- models/user.go
+++ models/user.go
@@ -2,14 +2,14 @@ package models
 
 type User struct {
 	UID            string   `json:"uid"`
-	FirstName      string   `json:"first_name"`
-	SecondName     string   `json:"second_name"`
-	LastName       string   `json:"last_name"`
-	SecondLastName string   `json:"second_last_name"`
-	Gender         string   `json:"gender"`
-	Nationality    string   `json:"nationality"`
-	NationalID     string   `json:"national_id"`
-	Email          string   `json:"email"`
-	Type           string   `json:"type"`
+	FirstName      *string  `json:"first_name"`
+	SecondName     *string  `json:"second_name"`
+	LastName       *string  `json:"last_name"`
+	SecondLastName *string  `json:"second_last_name"`
+	Gender         *string  `json:"gender"`
+	Nationality    *string  `json:"nationality"`
+	NationalID     *string  `json:"national_id"`
+	Email          *string  `json:"email"`
+	Type           *string  `json:"type"`
 	Roles          []string `json:"roles"`
 }
