7ede45dea2a2461ba2ce6ef30892010bc39293fc
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-08-13 17:32:06 -0400

    feat: extends the pagination struct

--- models/pagination.go
+++ models/pagination.go
@@ -1,6 +1,8 @@
 package models
 
 type Pagination struct {
-	Page         int `json:"page"`
-	ItemsPerPage int `json:"items_per_page"`
+	Page         int  `json:"page"`
+	ItemsPerPage int  `json:"items_per_page"`
+	TotalItems   *int `json:"total_items"`
+	TotalPages   *int `json:"total_pages"`
 }
