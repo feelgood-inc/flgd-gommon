60e07520a66fb6c33e9ebb150a88e07a4e182a99
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-08-29 13:45:44 -0400

    refactor: removes timezone from postgres adapter

--- adapters/postgres.go
+++ adapters/postgres.go
@@ -10,13 +10,12 @@ import (
 )
 
 func CreatePostgresClient(ctx context.Context, cfg *config.Config) *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=%s",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
 		cfg.Postgres.Host,
 		cfg.Postgres.User,
 		cfg.Postgres.Password,
 		cfg.Postgres.DB,
 		cfg.Postgres.Port,
-		*cfg.Postgres.TimeZone,
 	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
