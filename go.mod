52baa6251e22149db746f321cc24e83873f79874
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-09-18 01:16:34 -0300

    feat: set new kafka writer util

--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	cloud.google.com/go/firestore v1.6.1 // indirect
 	cloud.google.com/go/iam v0.1.1 // indirect
 	cloud.google.com/go/storage v1.21.0 // indirect
+	github.com/avast/retry-go v3.0.0+incompatible // indirect
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- utils/kafka/writer.go
+++ utils/kafka/writer.go
@@ -1,7 +1,12 @@
 package kafka
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/avast/retry-go"
 	"github.com/feelgood-inc/flgd-gommon/logger"
+	"github.com/feelgood-inc/flgd-gommon/models"
 	"github.com/segmentio/kafka-go"
 )
 
@@ -17,4 +22,47 @@ func NewWriter(writer kafka.Writer, log logger.Logger) *kafka.Writer {
 		w.ErrorLogger = kafka.LoggerFunc(log.Errorf)
 	}
 	return w
+}
+
+func WriteToTopicInBackground(
+	ctx context.Context,
+	writer *kafka.Writer,
+	topic string,
+	key string,
+	value models.KafkaMessage,
+	log logger.Logger,
+) error {
+	if writer == nil {
+		log.Errorf("writer is nil")
+		return errors.New("writer is nil")
+	}
+
+	messageAsBytes, err := json.Marshal(value)
+	if err != nil {
+		log.Errorf("Error marshalling message: %v", err)
+		return err
+	}
+
+	go func(cx context.Context, msg []byte, topic string, key string) {
+		if err2 := retry.Do(func() error {
+			err2 := writer.WriteMessages(ctx, kafka.Message{
+				Key:   []byte(key),
+				Value: msg,
+				Topic: topic,
+			})
+			if err2 != nil {
+				log.Errorf("Error writing message to topic: %v", err2)
+				return err2
+			}
+			return nil
+		},
+			retry.Attempts(3),
+			retry.DelayType(retry.BackOffDelay),
+			retry.Context(cx),
+		); err2 != nil {
+			log.Errorf("Error writing message to topic: %v", err2)
+		}
+	}(ctx, messageAsBytes, topic, key)
+
+	return nil
 }
