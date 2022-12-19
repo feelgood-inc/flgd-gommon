413713a0d796a7c8bacd881655c43f46d21e5b80
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2022-12-18 21:01:31 -0300

    feat: modifies auth to use cookie

--- middleware/echo/user_info.go
+++ middleware/echo/user_info.go
@@ -10,12 +10,22 @@ import (
 func SetUserInfo(withKey string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
+			var jwtToken string
+			authCookie, err := ctx.Request().Cookie("Authorization")
+			if err != nil {
+				return ctx.JSON(http.StatusUnauthorized, "Unauthorized")
+			} else {
+				jwtToken = authCookie.Value
+			}
+
 			authHeader := ctx.Request().Header.Get("Authorization")
 			if authHeader == "" {
 				return ctx.JSON(http.StatusUnauthorized, "Unauthorized")
+			} else {
+				jwtToken = authHeader
 			}
 
-			decodedToken, _ := jwt.ParseWithClaims(authHeader, &models.FeelgoodJWTClaims{}, nil)
+			decodedToken, _ := jwt.ParseWithClaims(jwtToken, &models.FeelgoodJWTClaims{}, nil)
 			user := models.User{
 				UID:   decodedToken.Claims.(*models.FeelgoodJWTClaims).Claims.UID,
 				Email: &decodedToken.Claims.(*models.FeelgoodJWTClaims).Claims.Email,
