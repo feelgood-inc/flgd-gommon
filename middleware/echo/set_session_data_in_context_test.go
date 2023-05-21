a8423ef5c4049372d882fe3f36d2e4917bf26517
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2023-05-21 00:37:23 -0400

    fix: claims OK

---
+++ middleware/echo/set_session_data_in_context_test.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"testing"
+
+	"github.com/feelgood-inc/flgd-gommon/models"
+	"github.com/golang-jwt/jwt"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecodesTokenCorrectly(t *testing.T) {
+	const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGFpbXMiOnsiZXhwIjoxNjg0NzMwMTQ3LCJyb2xlcyI6WyJhZG1pbiIsImludGVybmFsIl0sInNlcnZpY2VfbmFtZSI6InRlc3QiLCJ1c2VyX3R5cGUiOiJhZG1pbiJ9fQ.LmLpBfGFTJdtyNJucAaD2fikU6SlSQj90flGctXDKJs"
+
+	decodedToken, err := jwt.ParseWithClaims(token, &models.FeelgoodJWTClaims{}, nil)
+
+	print(decodedToken.Claims.(*models.FeelgoodJWTClaims).Claims.Roles)
+
+	assert.Error(t, err)
+	assert.Contains(t, decodedToken.Claims.(*models.FeelgoodJWTClaims).Claims.Roles, "admin")
+}
