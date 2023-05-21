99eb3035af46ba81335aaf535a385f46a883d184
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2023-05-20 23:40:06 -0400

    feat: adds slice utils

--- utils/slices.go
+++ utils/slices.go
@@ -1,6 +1,6 @@
 package utils
 
-func contains(slice []string, item string) bool {
+func sliceContains(slice []string, item string) bool {
 	for _, s := range slice {
 		if s == item {
 			return true
