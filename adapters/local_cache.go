cb76b762d50879df0300464f84032f92b6c647f7
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2023-05-24 21:03:13 -0400

    refactor: rename local cache func

--- adapters/local_cache.go
+++ adapters/local_cache.go
@@ -2,6 +2,6 @@ package adapters
 
 import "github.com/dgraph-io/ristretto"
 
-func CreateLocalCacheWithConfig(config *ristretto.Config) (*ristretto.Cache, error) {
+func CreateLocalCache(config *ristretto.Config) (*ristretto.Cache, error) {
 	return ristretto.NewCache(config)
 }
