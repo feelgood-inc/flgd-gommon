3cccc12fb030aa69f18b7c574a66a6d13d622567
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2023-05-24 20:59:06 -0400

    feat: get token function

--- utils/auth.go
+++ utils/auth.go
@@ -1,9 +1,23 @@
 package utils
 
 import (
+	"context"
+	"fmt"
 	"github.com/dgraph-io/ristretto"
+	httpclient "github.com/feelgood-inc/flgd-gommon/http_client"
+	"github.com/feelgood-inc/flgd-gommon/models"
+	"github.com/go-resty/resty/v2"
 	"net/http"
+	"time"
 )
+
+type GetInternalTokenConfig struct {
+	RetryCount    int
+	RetryWaitTime time.Duration
+	ServiceName   string
+	Debug         bool
+	AuthMSHost    string
+}
 
 func CheckIfRequestIsAuthenticated(req *http.Request) (bool, *string) {
 	isAuthenticated := false
@@ -34,4 +48,45 @@ func CheckIfRequestIsAuthenticated(req *http.Request) (bool, *string) {
 
 func SetInternalAuthTokenInLocalCache(localCache *ristretto.Cache, token string) {
 	localCache.Set("internal_auth_token", token, 1)
+}
+
+func GetInternalToken(ctx context.Context, config *GetInternalTokenConfig) (string, error) {
+	customClient := resty.New().
+		SetHeaders(map[string]string{
+			"Content-Type":     "application/json",
+			"Accept":           "application/json",
+			"User-Agent":       "flgd-resty-client",
+			"X-Application-ID": config.ServiceName,
+		}).
+		SetRetryCount(config.RetryCount).
+		SetRetryWaitTime(config.RetryWaitTime)
+
+	if config.Debug {
+		customClient.SetDebug(true)
+	}
+
+	// Obtain the token from the auth microservice
+	resp, err := customClient.
+		R().
+		SetContext(ctx).
+		SetBody(map[string]string{
+			"service_name": config.ServiceName,
+		}).
+		Post(config.AuthMSHost + "/auth/login-internal-ms")
+
+	if err != nil {
+		return "", err
+	}
+
+	if resp.IsError() {
+		return "", fmt.Errorf("failed with status code: %d and body: %s", resp.StatusCode(), resp.Body())
+	}
+
+	var loginData models.LoginData
+	err = httpclient.FeelgoodResponseToStruct(resp, &loginData)
+	if err != nil {
+		return "", err
+	}
+
+	return loginData.Token, nil
 }
